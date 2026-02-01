0eebbf0fa68b23de39cad5ead665e23af4828307
Zamorax2004 <ignacio.zamora@correo.unimet.edu.ve>
2026-02-01 13:34:40 -0400

    Setup Inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,36 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MemoryGameApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MemoryGameApp extends StatelessWidget {
+  const MemoryGameApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Flutter Memoria',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.indigo, useMaterial3: true),
+      home: const GamePage(),
     );
   }
+}
+
+class CardModel {
+  final int id;
+  final IconData icon;
+  bool isFlipped;
+  bool isMatched;
+
+  CardModel({
+    required this.id,
+    required this.icon,
+    this.isFlipped = false,
+    this.isMatched = false,
+  });
 }
